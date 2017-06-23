318ed0096a05b7cfa941f8b317262f23626d0400
Mariel Salcedo <Mariel Salcedo>
2017-06-22 16:10:40 -1000

    found ids for btns

--- js/app.js
+++ js/app.js
@@ -31,13 +31,30 @@
    * on this dom element, set the className to the value of desiredClass
    */
 
-
+   /*function setMyLightClass(event, desiredClass){
+    let
+   }
+*/
   /*
    * Declare 15 constants btn1, btn2, btn3, ...
    * use a dom element selector method to assign each const
    * to the button that has an id of the same name.
    */
-
+   const btn1 = document.getElementById("btn1");
+   const btn2 = document.getElementById("btn2");
+   const btn3 = document.getElementById("btn3");
+   const btn4 = document.getElementById("btn4");
+   const btn5 = document.getElementById("btn5");
+   const btn6 = document.getElementById("btn6");
+   const btn7 = document.getElementById("btn7");
+   const btn8 = document.getElementById("btn8");
+   const btn9 = document.getElementById("btn9");
+   const btn10 = document.getElementById("btn10");
+   const btn11 = document.getElementById("btn11");
+   const btn12 = document.getElementById("btn12");
+   const btn13 = document.getElementById("btn13");
+   const btn14 = document.getElementById("btn14");
+   const btn15= document.getElementById("btn15");
 
   /*
    * Add a click event listener to btn1
