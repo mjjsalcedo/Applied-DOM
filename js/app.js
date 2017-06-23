4de6755ff51163a205fe5d348343e7c0388cb9a8
Mariel Salcedo <Mariel Salcedo>
2017-06-22 18:20:23 -1000

    #9 done

--- js/app.js
+++ js/app.js
@@ -166,6 +166,7 @@
    *   and passing two aditional arguments, event and 'light-green'
    */
 
+   btn9.addEventListener("click", (event) => setMyLightClass.call(btn9, event, "light-green"));
 
   /*
    * Declare a new const named setLight10Green
